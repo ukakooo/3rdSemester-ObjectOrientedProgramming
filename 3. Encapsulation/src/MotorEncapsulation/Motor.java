49d092251d8d0f5347bedab27cffcf1b4a2781c3
ukakooo <herconaryangga15@gmail.com>
2025-09-12 21:17:52 +0700

    Full JS3 Commit

--- 3. Encapsulation/src/MotorEncapsulation/Motor.java
+++ 3. Encapsulation/src/MotorEncapsulation/Motor.java
@@ -5,6 +5,35 @@ public class Motor {
     private int kecepatan = 0;
     private boolean kontakOn = false;
 
+    public void nyalakanMesin() {
+        kontakOn = true;
+    }
+
+    public void matikanMesin() {
+        kontakOn = false;
+        kecepatan = 0;
+    }
+
+    public void tambahKecepatan() {
+        if (kontakOn) {
+            if (kecepatan >= 100) {
+                System.out.println("Kecepatan sudah maksimal\n");
+            } else {
+                kecepatan += 5;
+            }
+        } else {
+            System.out.println("Kecepatan tidak bisa ditambahkan karena Mesin Off.\n");
+        }
+    }
+
+    public void kurangiKecepatan() {
+        if (kontakOn) {
+            kecepatan -= 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa ditambahkan karena Mesin Off.\n");
+        }
+    }
+
     public void printStatus () {
         if (kontakOn) {
             System.out.println("Kontak On");
