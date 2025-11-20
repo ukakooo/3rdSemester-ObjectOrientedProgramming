80d6a141e878742c3a38614f2add49989ed6bcec
ukakooo <herconaryangga15@gmail.com>
2025-11-21 00:03:37 +0700

    JS13 NetBeans

---
+++ 13. GUI/NetBeans/ExperimentGUI2/src/main/java/experimentgui2/ExperimentGUI2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package experimentgui2;
+
+/**
+ *
+ * @author PC
+ */
+public class ExperimentGUI2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
