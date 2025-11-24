c9fc6db2525140d2e17d3958210ba20264eac93c
ukakooo <herconaryangga15@gmail.com>
2025-11-24 16:44:35 +0700

    another commit but idk what changed

--- Quiz2/src/Tower.java
+++ Quiz2/src/Tower.java
@@ -2,6 +2,6 @@ public class Tower implements Destroyable{
     @Override
     public void destroyed() {
         // TODO Auto-generated method stub
-        System.out.println("What did I do");
+        System.out.println("");
     }
 }
